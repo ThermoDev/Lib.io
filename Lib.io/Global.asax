5118a20b914ec98a112292809bb12f423faee1dd
ThermoDev <michael.pegios@hotmail.com>
2020-08-15 13:08:29 +1000

    Add project files.

---
+++ Lib.io/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Lib.io.MvcApplication" Language="C#" %>
---
+++ Lib.io/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Lib.io.Startup))]
+namespace Lib.io
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

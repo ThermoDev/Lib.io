1c556aef2761e32be8c70b7fb83348217606940c
ThermoDev <michael.pegios@hotmail.com>
2020-08-18 18:37:07 +1000

    Adding Bootbox for Confirm Dialog

--- Lib.io/App_Start/BundleConfig.cs
+++ Lib.io/App_Start/BundleConfig.cs
@@ -18,6 +18,7 @@ namespace Lib.io {
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
+                      "~/Scripts/bootbox.js",
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(

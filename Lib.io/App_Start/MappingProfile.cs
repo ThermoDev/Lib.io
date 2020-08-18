6ba13eea3363e0fdf26d9583c13d0eb0da96dcf9
ThermoDev <michael.pegios@hotmail.com>
2020-08-18 18:08:15 +1000

    Building RESTful Services for Books using ASP.NET Web API

--- Lib.io/App_Start/MappingProfile.cs
+++ Lib.io/App_Start/MappingProfile.cs
@@ -7,12 +7,14 @@ using Lib.io.Dtos;
 using Lib.io.Models;
 
 namespace Lib.io.App_Start {
-    public class MappingProfile : Profile{
+    public class MappingProfile : Profile {
         public MappingProfile() {
             // Automapper uses reflection to scan these types,
             // find their properties, and maps them based on their name
-            Mapper.CreateMap<Member, MemberDto>();
+            Mapper.CreateMap<Member, MemberDto>().ForMember(c => c.Id, opt => opt.Ignore()); ;
             Mapper.CreateMap<MemberDto, Member>();
+            Mapper.CreateMap<Book, BookDto>().ForMember(b => b.Id, opt => opt.Ignore()); ;
+            Mapper.CreateMap<BookDto, Book>();
         }
     }
 }
---
+++ Lib.io/Dtos/BookDto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Lib.io.Dtos {
+    public class BookDto {
+        public int Id { get; set; }
+
+        [Required]
+        public String Name { get; set; }
+
+        [Required]
+        public byte GenreId { get; set; }
+
+        [Required]
+        public DateTime? ReleaseDate { get; set; }
+
+        public DateTime DateAdded { get; set; }
+
+        [Required]
+        [Range(1, 20)]
+        public int NumberInStock { get; set; }
+    }
+}

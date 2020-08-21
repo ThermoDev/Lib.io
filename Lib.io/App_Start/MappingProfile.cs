9f449909a31e70e22486b7a5201e60fa8a653d9b
ThermoDev <michael.pegios@hotmail.com>
2020-08-21 17:35:08 +1000

    Fix Member Mapping

--- Lib.io/App_Start/MappingProfile.cs
+++ Lib.io/App_Start/MappingProfile.cs
@@ -11,10 +11,14 @@ namespace Lib.io.App_Start {
         public MappingProfile() {
             // Automapper uses reflection to scan these types,
             // find their properties, and maps them based on their name
-            Mapper.CreateMap<Member, MemberDto>().ForMember(c => c.Id, opt => opt.Ignore()); ;
-            Mapper.CreateMap<MemberDto, Member>();
-            Mapper.CreateMap<Book, BookDto>().ForMember(b => b.Id, opt => opt.Ignore()); ;
-            Mapper.CreateMap<BookDto, Book>();
+            Mapper.CreateMap<Member, MemberDto>();
+            Mapper.CreateMap<MemberDto, Member>().ForMember(m => m.Id, opt => opt.Ignore());
+
+            Mapper.CreateMap<Book, BookDto>();
+            Mapper.CreateMap<BookDto, Book>().ForMember(b => b.Id, opt => opt.Ignore());
+
+            Mapper.CreateMap<MembershipType, MembershipTypeDto>();
+            Mapper.CreateMap<MembershipTypeDto, MembershipType>();
         }
     }
 }
